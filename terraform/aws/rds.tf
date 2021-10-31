3eb8bccb4eb9b1fb7487394422156b5fa1e8164d
tronxd <tron47@gmail.com>
2021-10-31 17:28:01 +0200

    ViolationRecommendations DAL

---
+++ terraform/aws/rds.tf
@@ -0,0 +1,43 @@
+resource "aws_rds_cluster" "fail" {
+  allocated_storage       = 10
+}
+
+resource "aws_rds_cluster" "pass1" {
+  allocated_storage       = 10
+  backup_retention_period = 1
+}
+
+resource "aws_rds_cluster" "pass2" {
+  allocated_storage       = 10
+  backup_retention_period = 15
+}
+
+resource "aws_rds_cluster" "pass3" {
+  allocated_storage       = 10
+  backup_retention_period = 15
+}
+
+resource "aws_rds_cluster" "pass4" {
+  allocated_storage       = 10
+  backup_retention_period = 15
+}
+
+resource "aws_rds_cluster" "pass5" {
+  allocated_storage       = 10
+  backup_retention_period = 15
+}
+
+resource "aws_rds_cluster" "pass6" {
+  allocated_storage       = 10
+  backup_retention_period = 25
+}
+
+resource "aws_rds_cluster" "pass7" {
+  allocated_storage       = 10
+  backup_retention_period = 25
+}
+
+resource "aws_rds_cluster" "pass8" {
+  allocated_storage       = 10
+  backup_retention_period = 25
+}
